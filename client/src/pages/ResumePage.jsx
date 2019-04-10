a6a133200c5a2ac8f77751abd43bc3733d74e146
FX-Wood <fxwood2@gmail.com>
2019-04-09 19:26:16 -0700

    Adding ResumePage component

---
+++ client/src/pages/ResumePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from '../components';
+class ResumePage extends Component {
+    render() {
+        return (
+            <div className="ResumePage">
+                <Nav />
+                <h1>Resume</h1>
+            </div>
+        )
+    }
+}
+
+export default ResumePage
