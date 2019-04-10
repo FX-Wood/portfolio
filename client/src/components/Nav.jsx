d193216b2c09350622520ba23aa3c1da78599c88
FX-Wood <fxwood2@gmail.com>
2019-04-09 21:12:06 -0700

    Nav now points root route to home

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Nav = props => {
     return (
         <nav className="Nav">
-            <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/resume">Resume</Link>
             <Link to="/contact">Contact</Link>
