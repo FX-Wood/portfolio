30904f86ce9431677d92584379c6016f9c85f6bb
FX-Wood <fxwood2@gmail.com>
2019-04-09 23:30:27 -0700

    Added contact page and styling

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
-
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'react-bootstrap/FormGroup';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 class ContactPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        console.log(this.state)
+    }
+    
+
     render() {
         return (
-            <div className="ContactPage">
+            <div className="ContactPage page">
                 <Nav />
-                <h1>ContactPage</h1>
+                <div className="content">
+                    <Container>
+                        <Form onSubmit={this.handleSubmit} >
+                            <FormGroup className="name">
+                                <Form.Label>Name: </Form.Label>
+                                <Form.Control type="text" name="name" onChange={this.handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Form.Label>Email: </Form.Label>
+                                <Form.Control type="email" name="email" onChange={this.handleChange} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Form.Label>Message: </Form.Label>
+                                <Form.Control as="textarea" name="message" rows="4" onChange={this.handleChange} />
+                            </FormGroup>
+                            <Button type="submit">Send</Button>
+                        </Form>
+                    </Container>
+                </div>
             </div>
         )
     }
