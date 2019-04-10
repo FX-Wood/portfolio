05aaa4f9991fb2d7d9fb51e2e52a477264f2fe83
FX-Wood <fxwood2@gmail.com>
2019-04-09 22:29:50 -0700

    Nav and styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav as bootNav } from 'react-bootstrap';
 
 const Nav = props => {
     return (
-        <nav className="Nav">
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/resume">Resume</Link>
-            <Link to="/contact">Contact</Link>
-        </nav>
+        <bootNav as="nav" >
+            <bootNav.Item><Link to="/">Home</Link></bootNav.Item>
+            <bootNav.Item><Link to="/projects">Projects</Link></bootNav.Item>
+            <bootNav.Item><Link to="/resume">Resume</Link></bootNav.Item>
+            <bootNav.Item><Link to="/contact">Contact</Link></bootNav.Item>
+        </bootNav>
     )
 }
 
