c8adaaa80fd02f8cfb3f27db0c79c2c4935f0d37
FX-Wood <fxwood2@gmail.com>
2019-04-09 19:46:55 -0700

    Hooked up pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Nav from './components/Nav';
+import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import ResumePage from './pages/ResumePage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
-    
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <h1>App</h1>
+          <Route exact path="/" component={HomePage} />
+          <Route path='/projects' component={ProjectsPage} />
+          <Route path='/resume' component={ResumePage} />
+          <Route path='/contact' component={ContactPage} />
+        </div>
+      </Router>
     );
   }
 }
