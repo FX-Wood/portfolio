c03fbc4e3dfdf809a3aaf7858da862053b4188ae
FX-Wood <fxwood2@gmail.com>
2019-04-10 17:50:22 -0700

    Experimenting with bootstrap carousel

--- client/src/components/ProjectSlide.jsx
+++ client/src/components/ProjectSlide.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
-const ProjectCard = props => {
+const imageStyle = {
+    width: '80vw',
+    height: 300
+}
+
+const ProjectSlide = props => {
+    console.log(props.image)
     return (
-        <Carousel.Item className="Project">
-            <img src={props.image} alt="Project detail"/>
-            <Carousel.Caption>
-            <h2>{props.name}</h2>
-            <p>{props.description}</p>
-            </Carousel.Caption>
-        </Carousel.Item>
+        <>
+        {/* <img src={props.image} alt=""/> */}
+            <Carousel.Item>
+                {/* <img style={imageStyle} src='https://via.placeholder.com/300' alt="Project detail"/> */}
+                <Image
+                    className="d-block w-100"
+                    src={props.image}
+                    alt="Third slide"
+                />
+                {/* <Carousel.Caption>
+                <h2>{props.name}</h2>
+                <p>{props.description}</p>
+                </Carousel.Caption> */}
+            </Carousel.Item>
+        </>
     )
 }
 
-export default ProjectCard
+export default ProjectSlide
--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -1,46 +1,96 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
-import CardDeck from 'react-bootstrap/CardDeck';
 import Container from 'react-bootstrap/Container';
+// mobile
+import CardDeck from 'react-bootstrap/CardDeck';
 import ProjectCard from '../components/ProjectCard';
+
+// desktop
+import Carousel from 'react-bootstrap/Carousel';
+// see slide function below
+
+// project images
+import dombleImg from '../img/kingdom-select.png';
+import extraImg from '../img/create-cards.png';
+import propsImg from '../img/props-login.png';
 
 
 var projects = [
     {
         name: "Domblenon",
-        image: "https://via.placeholder.com/300",
+        image: dombleImg,
         description: "Dominion implementation",
         descriptionLong: "Simple implementation of the deck-building game Dominion.",
         technologies: "Express.js, ES6 classes, Google Charts",
     },
     {
         name: "Extra",
-        image: "https://via.placeholder.com/300",
+        image: extraImg,
         description: "Flash cards as a service",
         descriptionLong: "Platform where folks can make and review flash cards to help with whatever they are studying",
         technologies: "Node.js, Express.js, PostgreSQL, Sequelize ORM, Heroku"
     },
     {
         name: "Props",
-        image: "https://via.placeholder.com/300",
+        image: propsImg,
         description: "Workplace engagement tool",
         descriptionLong: "A platform for employees to communicate and give their respects to each other",
         technologies: "React, Node.js, Express.js, MongoDB, Mongoose ODM, Heroku, reCharts"
     }
 ]
 
+const slide = project => (
+    <Carousel.Item>
+        <img
+            className="d-block w-100"
+            src={project.image}
+            alt="Third slide"
+        />
+        <Carousel.Caption>
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        </Carousel.Caption>
+    </Carousel.Item>
+)
+
 class ProjectsPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            mobile: false
+        }
+    }
     render() {
+        let content;
+        if (this.state.mobile === true) {
+            content = (
+                // mobile content 
+                <div className="content">
+                    <Container>
+                        <CardDeck>
+                            {projects.map(project => <ProjectCard {...project}/>)}
+                        </CardDeck>
+                    </Container>
+                </div>
+            )
+        } else {
+            content = (
+                <div className="content">
+                <Container>
+                        <Carousel>
+                            {projects.map(slide)}
+                        </Carousel>
+                </Container>
+                </div>
+            )
+        }
+        if (!content) {
+            content = <p>There was an error</p>
+        }
         return (
                 <div className="ProjectPage page">
                     <Nav />
-                    <div className="content">
-                        <Container>
-                            <CardDeck>
-                                {projects.map(project => <ProjectCard {...project}/>)}
-                            </CardDeck>
-                        </Container>
-                    </div>
+                    {content}
                 </div>
         )
     }
