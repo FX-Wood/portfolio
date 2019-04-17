c2488a181e16a98904c4e544395d10749d69a10f
FX-Wood <fxwood2@gmail.com>
2019-04-16 18:45:28 -0700

    Adding resume

--- client/src/pages/ResumePage.jsx
+++ client/src/pages/ResumePage.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
+import resume from './resume';
+import Container from 'react-bootstrap/Container'
 class ResumePage extends Component {
     render() {
         return (
             <div className="ResumePage">
                 <Nav />
                 <h1>Resume</h1>
+                <Container>
+                    {resume}
+                </Container>
             </div>
         )
     }
