c95a427f9afb877aa3f617ba25a2249d44bc2c2f
FX-Wood <fxwood2@gmail.com>
2019-06-04 23:04:46 -0700

    Resume refresh

--- client/src/pages/ResumePage.jsx
+++ client/src/pages/ResumePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
 import Container from 'react-bootstrap/Container';
-import resume from '../resume.png';
+import resume from '../img/resume.png';
 import reactGA from 'react-ga';
 
 class ResumePage extends Component {
@@ -12,7 +12,6 @@ class ResumePage extends Component {
         return (
             <div className="ResumePage">
                 <Nav />
-                <h1>Resume</h1>
                 <Container>
                     <img src={resume} alt="Resume"/>
                 </Container>
