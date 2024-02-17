b975b26944c5199c81f12ad4a713e210a1a4c9dc
FX Wood <fxwood2@gmail.com>
2024-02-16 17:55:14 -0800

    fix up images and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,9 @@ export default function RootLayout({ children }) {
             />
             <meta name="theme-color" content="#000000" />
             <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-            <link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicon-16x16.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
             <title>FX Wood</title>
           </head>
           <body>
