fdc7bf3a3bbc618cc6f3f18da38ba58da60d9d6d
FX Wood <fxwood2@gmail.com>
2024-02-16 18:08:32 -0800

    fix styles and icons

--- src/app/components/SocialMediaLinks.jsx
+++ src/app/components/SocialMediaLinks.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import octocatDark from '../icons/github-white-fill.png';
-import inDark from '../icons/linkedin-white-fill.png';
 
 const SocialMediaLinks = () => {
     return (
         <>
-            <a href="https://www.linkedin.com/in/fx-wood"><img className="icon-link linkedin" src={inDark} alt="Linkedin profile"/></a>
-            <a href="https://www.github.com/fx-wood"><img className="icon-link github" src={octocatDark} alt="Github profile"/></a>
+            <a href="https://www.linkedin.com/in/fx-wood"><img className="icon-link linkedin" src={ '/icon/linkedin-white-fill.png' } alt="Linkedin profile"/></a>
+            <a href="https://www.github.com/fx-wood"><img className="icon-link github" src={ '/icon/github-white-fill.png' } alt="Github profile"/></a>
         </>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
+import './index.css'
+import './App.css'
 export const metadata = {
     title: "fx.codes",
     description: "portfolio site for FX",
-    charSet: 'utf-8',
-    icon: '/media/favicon.ico'
+    charSet: 'utf-8'
 }
 
 export default function RootLayout({ children }) {
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
               content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
             <meta name="theme-color" content="#000000" />
-            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+            <link rel="manifest" href="/favicon/manifest.json" />
             <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
