4c0e85084a540f36e4c27986506f8f1b62768f98
FX Wood <fxwood2@gmail.com>
2024-02-18 20:51:06 -0800

    fix projects link

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,7 +7,7 @@ export default function ContactPage() {
     return (
         <main className="contact-main">
                 <Container>
-                    <p>check out my <a to="/">projects</a></p>
+                    <p>check out my <a href="/">projects</a></p>
                     <p>send me an <a href="mailto:fxwood.work@gmail.com" target='_blank'>email</a></p>
                     <p>or connect with me on <a href="https://linkedin.com/in/fx-wood"> linkedin!</a></p>
                     <SocialMediaLinks />
