db58f9d3d9a8361ea5f6679f949950eab860d44f
FX Wood <fxwood2@gmail.com>
2024-02-23 20:00:32 -0800

    take another stab at the url

--- src/middleware.js
+++ src/middleware.js
@@ -8,9 +8,9 @@ export function middleware(request) {
 		console.log('path', request.path)
 		console.log(request.nextUrl)
 		if (request.headers['x-forwarded-proto'] !== 'https') {
-			const redirectURL = new URL(`https://${request.headers['host']}${request.url}`)
-			console.log('redirectURL', redirectURL)
-			return NextResponse.redirect(redirectURL)
+			const redirectURL = new URL(`https://${request.headers['host']}${request.nextUrl.pathname}`)
+			console.log('redirectURL', redirectURL.toString())
+			return NextResponse.redirect(redirectURL.toString())
 		}
 	}
 }
