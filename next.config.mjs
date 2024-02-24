ee730322f8cd0be6f0cd1b25bacecdf8d6f018fa
FX Wood <fxwood2@gmail.com>
2024-02-23 20:39:39 -0800

    prefer https

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist'
-}
+  distDir: "dist",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
-
+export default nextConfig;
