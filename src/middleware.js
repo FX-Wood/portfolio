29f7fd43d19dfc0c13d25db843c0cff4ebcc581a
FX Wood <fxwood2@gmail.com>
2024-02-23 19:38:40 -0800

    find url value

--- src/middleware.js
+++ src/middleware.js
@@ -4,8 +4,12 @@ export function middleware(request) {
 	if (process.env.NODE_ENV === 'production') {
 		// redirect https
 		console.log(request.headers)
+		console.log(request.url)
+		console.log('path', request.path)
 		if (request.headers['x-forwarded-proto'] !== 'https') {
-			return NextResponse.redirect(`https://${request.headers['host']}${request.url}`)
+			const redirectURL = new URL(`https://${request.headers['host']}${request.url}`)
+			console.log('redirectURL', redirectURL)
+			return NextResponse.redirect(redirectURL)
 		}
 	}
 }
