d26ac594347a4ae02e934363d549ad8feb558bea
FX Wood <fxwood2@gmail.com>
2024-02-23 20:18:29 -0800

    try getting headers

--- src/middleware.js
+++ src/middleware.js
@@ -7,8 +7,10 @@ export function middleware(request) {
 		console.log(request.url)
 		console.log('path', request.path)
 		console.log(request.nextUrl)
-		if (request.headers['x-forwarded-proto'] !== 'https') {
-			const redirectURL = new URL(`https://${request.headers['host']}${request.nextUrl.pathname}`)
+		console.log('xfp', request.headers.get('x-forwarded-proto'))
+		console.log('host', request.headers.get('host'))
+		if (request.headers.get('x-forwarded-proto') !== 'https') {
+			const redirectURL = new URL(`https://${request.headers.get('host')}${request.nextUrl.pathname}`)
 			console.log('redirectURL', redirectURL.toString())
 			return NextResponse.redirect(redirectURL.toString())
 		}
